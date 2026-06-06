fdf1d841eaa1cc8f702e1669d94e895fa9d10c74
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-06-06 11:17:48 +0100

    Update AddDoctor.Designer.cs

--- AddDoctor.Designer.cs
+++ AddDoctor.Designer.cs
@@ -1,7 +1,7 @@
 ﻿namespace HMS___CA_ONE
  {
     partial class AddDoctor
-        /// Required designer
+        /// Required designe
      le.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
